5db32bdf452b3856fde8cda60fe9f65047e817a6
XiaoYeZi <qaz6750@outlook.com>
2025-01-18 16:34:38 +0800

    Silicon: 8998: AcpiPlatformUpdateLib: Update the ACPI table based on actual conditions
    Signed-off-by: XiaoYeZi <qaz6750@outlook.com>

--- Silicon/QC/Msm8998/QcomPkg/Library/AcpiPlatformUpdateLib/AcpiPlatformUpdateLib.c
+++ Silicon/QC/Msm8998/QcomPkg/Library/AcpiPlatformUpdateLib/AcpiPlatformUpdateLib.c
@@ -27,22 +27,22 @@ PlatformUpdateAcpiTables(VOID)
   ARM_MEMORY_REGION_DESCRIPTOR_EX TGCMRegion;
 
   UINT32                              SOID  = 0;
-  UINT32                              STOR  = 0x1;
+//  UINT32                              STOR  = 0x1;
   UINT32                              SIDV  = 0;
   UINT16                              SDFE  = 0;
   UINT16                              SIDM  = 0;
-  UINT32                              SUFS  = 0xFFFFFFFF;
-  UINT32                              PUS3  = 0x0; // If your device have ufs3 should enable this.
-  UINT32                              SUS3  = 0xFFFFFFFF;
-  UINT32                             *pSIDT = (UINT32 *)0x784130;
-  UINT32                              SIDT  = (*pSIDT & 0xFF00000) >> 20;
+//  UINT32                              SUFS  = 0xFFFFFFFF;
+//  UINT32                              PUS3  = 0x0; // If your device have ufs3 should enable this.
+//  UINT32                              SUS3  = 0xFFFFFFFF;
+//  UINT32                             *pSIDT = (UINT32 *)0x784130;
+//  UINT32                              SIDT  = (*pSIDT & 0xFF00000) >> 20;
   UINT32                              SOSN1 = 0;
   UINT32                              SOSN2 = 0;
-  UINT32                              TPMA  = 0x1;
-  UINT32                              TDTV  = 0x6654504D;
+//  UINT32                              TPMA  = 0x1;
+//  UINT32                              TDTV  = 0x6654504D;
   UINT64                              SOSI  = 0;
   UINT32                              PRP0  = 0;
-  UINT32                              PRP1  = 0;
+//  UINT32                              PRP1  = 0;
   CHAR8                               SIDS[EFICHIPINFO_MAX_ID_LENGTH] = {0};
   EFI_PLATFORMINFO_PLATFORM_INFO_TYPE PlatformInfo;
   UINT32                              RMTB = 0;
@@ -142,34 +142,31 @@ PlatformUpdateAcpiTables(VOID)
   DEBUG((EFI_D_WARN, "Chip Info Address: 0x%x\n", SOSI));
   DEBUG((EFI_D_WARN, "Platform Subtype: %d\n", PLST));
 
-  // Temporary
-  /*
   UpdateNameAslCode(SIGNATURE_32('S', 'O', 'I', 'D'), &SOID, 4);
-  UpdateNameAslCode(SIGNATURE_32('S', 'T', 'O', 'R'), &STOR, 4);
+//  UpdateNameAslCode(SIGNATURE_32('S', 'T', 'O', 'R'), &STOR, 4);
   UpdateNameAslCode(SIGNATURE_32('S', 'I', 'D', 'V'), &SIDV, 4);
   UpdateNameAslCode(SIGNATURE_32('S', 'V', 'M', 'J'), &SVMJ, 2);
   UpdateNameAslCode(SIGNATURE_32('S', 'V', 'M', 'I'), &SVMI, 2);
   UpdateNameAslCode(SIGNATURE_32('S', 'D', 'F', 'E'), &SDFE, 2);
   UpdateNameAslCode(SIGNATURE_32('S', 'I', 'D', 'M'), &SIDM, 2);
-  UpdateNameAslCode(SIGNATURE_32('S', 'U', 'F', 'S'), &SUFS, 4);
-  UpdateNameAslCode(SIGNATURE_32('P', 'U', 'S', '3'), &PUS3, 4);
-  UpdateNameAslCode(SIGNATURE_32('S', 'U', 'S', '3'), &SUS3, 4);
-  UpdateNameAslCode(SIGNATURE_32('S', 'I', 'D', 'T'), &SIDT, 4);
+//  UpdateNameAslCode(SIGNATURE_32('S', 'U', 'F', 'S'), &SUFS, 4);
+//  UpdateNameAslCode(SIGNATURE_32('P', 'U', 'S', '3'), &PUS3, 4);
+//  UpdateNameAslCode(SIGNATURE_32('S', 'U', 'S', '3'), &SUS3, 4);
+//  UpdateNameAslCode(SIGNATURE_32('S', 'I', 'D', 'T'), &SIDT, 4);
   UpdateNameAslCode(SIGNATURE_32('S', 'O', 'S', 'N'), &SOSN, 8);
-  UpdateNameAslCode(SIGNATURE_32('P', 'L', 'S', 'T'), &PLST, 4);
+//  UpdateNameAslCode(SIGNATURE_32('P', 'L', 'S', 'T'), &PLST, 4);
   UpdateNameAslCode(SIGNATURE_32('R', 'M', 'T', 'B'), &RMTB, 4);
   UpdateNameAslCode(SIGNATURE_32('R', 'M', 'T', 'X'), &RMTX, 4);
   UpdateNameAslCode(SIGNATURE_32('R', 'F', 'M', 'B'), &RFMB, 4);
   UpdateNameAslCode(SIGNATURE_32('R', 'F', 'M', 'S'), &RFMS, 4);
   UpdateNameAslCode(SIGNATURE_32('R', 'F', 'A', 'B'), &RFAB, 4);
   UpdateNameAslCode(SIGNATURE_32('R', 'F', 'A', 'S'), &RFAS, 4);
-  UpdateNameAslCode(SIGNATURE_32('T', 'P', 'M', 'A'), &TPMA, 4);
-  UpdateNameAslCode(SIGNATURE_32('T', 'D', 'T', 'V'), &TDTV, 4);
-  UpdateNameAslCode(SIGNATURE_32('T', 'C', 'M', 'A'), &TCMA, 4);
-  UpdateNameAslCode(SIGNATURE_32('T', 'C', 'M', 'L'), &TCML, 4);
-  UpdateNameAslCode(SIGNATURE_32('S', 'O', 'S', 'I'), &SOSI, 8);
+//  UpdateNameAslCode(SIGNATURE_32('T', 'P', 'M', 'A'), &TPMA, 4);
+//  UpdateNameAslCode(SIGNATURE_32('T', 'D', 'T', 'V'), &TDTV, 4);
+//  UpdateNameAslCode(SIGNATURE_32('T', 'C', 'M', 'A'), &TCMA, 4);
+//  UpdateNameAslCode(SIGNATURE_32('T', 'C', 'M', 'L'), &TCML, 4);
+//  UpdateNameAslCode(SIGNATURE_32('S', 'O', 'S', 'I'), &SOSI, 8);
   UpdateNameAslCode(SIGNATURE_32('P', 'R', 'P', '0'), &PRP0, 4);
-  UpdateNameAslCode(SIGNATURE_32('P', 'R', 'P', '1'), &PRP1, 4);
+//  UpdateNameAslCode(SIGNATURE_32('P', 'R', 'P', '1'), &PRP1, 4);
   UpdateNameAslCode(SIGNATURE_32('S', 'I', 'D', 'S'), &SIDS, EFICHIPINFO_MAX_ID_LENGTH);
-  */
 }
