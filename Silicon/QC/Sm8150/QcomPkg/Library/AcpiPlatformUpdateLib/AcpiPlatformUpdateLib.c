76982612d9a0f45634f3119f6683784f63a1b8c5
Rostislav Lastochkin <remtrik@mail.ru>
2025-01-30 19:48:34 +0300

    8150: Address UFS overvolting issue (#454)
    UFS3 with UFS2 settings - Successfully boots -> Irreversible harm to UFS chip due to overvoltage
    UFS2 with UFS3 settings - Doesn't boot -> No harm to UFS chip
    
    Based on that always assume device is UFS3 unless PcdStorageHasUFS3 is set to 0

--- Silicon/QC/Sm8150/QcomPkg/Library/AcpiPlatformUpdateLib/AcpiPlatformUpdateLib.c
+++ Silicon/QC/Sm8150/QcomPkg/Library/AcpiPlatformUpdateLib/AcpiPlatformUpdateLib.c
@@ -32,7 +32,7 @@ PlatformUpdateAcpiTables(VOID)
   UINT16                              SDFE  = 0;
   UINT16                              SIDM  = 0;
   UINT32                              SUFS  = 0xFFFFFFFF;
-  UINT32                              PUS3  = 0x0; // If your device have ufs3 should enable this.
+  UINT32                              PUS3  = FixedPcdGet32(PcdStorageHasUFS3);
   UINT32                              SUS3  = 0xFFFFFFFF;
   UINT32                             *pSIDT = (UINT32 *)0x784130;
   UINT32                              SIDT  = (*pSIDT & 0xFF00000) >> 20;
